aba660a8e7c21197ed42c33036bcbb0fa35459e4
orelhen <orelhen1995@gmail.com>
2024-03-18 14:12:32 +0200

    first commit

--- ShopWeb/wwwroot/js/product.js
+++ ShopWeb/wwwroot/js/product.js
@@ -21,6 +21,7 @@ function loadDataTable() {
                     return `<div class="w-75 btn-group" role="group">
                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i>Edit</a>
                     <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i>Delete</a>
+                    <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"><i class="bi bi-trash-fill"></i>Remove</a>
                      </div>`;
                 },
 
