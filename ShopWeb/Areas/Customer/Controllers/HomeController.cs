5031d9e10f2770664d2c43beaa4df2632b155d59
Guyaloosh <guyaloosh@gmail.com>
2024-04-06 18:46:45 +0300

    finish

--- ShopWeb/Areas/Customer/Controllers/HomeController.cs
+++ ShopWeb/Areas/Customer/Controllers/HomeController.cs
@@ -117,6 +117,11 @@ namespace ShopWeb.Areas.Customer.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
